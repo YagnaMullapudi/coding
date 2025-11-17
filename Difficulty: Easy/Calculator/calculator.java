7fe13091b86462c6be8c51bc96d9fa0e19b71bfb
YagnaMullapudi <yagnaganeswarimullapudi@gmail.com>
2025-11-17 23:36:14 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Calculator/calculator.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static void utility(int a, int b, int opr) {
+        // code here
+        if (opr == 1) {
+            System.out.print(String.valueOf(a + b));
+        } else if (opr == 2) {
+            System.out.print(String.valueOf(a - b));
+        } else if (opr == 3) {
+            System.out.print(String.valueOf(a * b));
+        } else {
+            System.out.print("Invalid Input");
+        }
+    }
+}
