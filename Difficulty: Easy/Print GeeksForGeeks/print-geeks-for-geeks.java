10a172d071c97910c9fe6362ab4e4f8e25be3420
YagnaMullapudi <yagnaganeswarimullapudi@gmail.com>
2025-12-11 21:27:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print GeeksForGeeks/print-geeks-for-geeks.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        // Prints Hel
+        // Prints GeeksForGeeks
+        System.out.println("GeeksForGeeks");
+    }
+}
