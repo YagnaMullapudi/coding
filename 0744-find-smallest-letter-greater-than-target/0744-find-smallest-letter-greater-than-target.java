65ab9405f51784fe20e12416fd1bcf1fc2a19d79
YagnaMullapudi <yagnaganeswarimullapudi@gmail.com>
2026-01-31 21:50:58 +0530

    Time: 76 ms (0.46%), Space: 46.5 MB (24.49%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -0,0 +1,18 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0, high = letters.length - 1;
+        char result = letters[0]; // default wrap-around
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (letters[mid] > target) {
+                result = letters[mid];
+                high = mid - 1; // search left part
+            } else {
+                low = mid + 1; // search right part
+            }
+        }
+        return result;
+    }
+}
