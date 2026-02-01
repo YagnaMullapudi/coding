032ac66a85f3028891bd4c41d05709a4cd5458aa
YagnaMullapudi <yagnaganeswarimullapudi@gmail.com>
2026-02-01 20:44:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input Floating Number/input-floating-number.java
@@ -0,0 +1,14 @@
+// Back-end complete function Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        float n = sc.nextFloat();   // Take float input
+        System.out.print(n * 10);   // Multiply by 10 and print
+        
+        sc.close();
+    }
+}
