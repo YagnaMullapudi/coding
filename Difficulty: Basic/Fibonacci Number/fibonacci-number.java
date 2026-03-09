e7aec6d1d32795c7a722feb4cdc970d47fdd4a17
YagnaMullapudi <yagnaganeswarimullapudi@gmail.com>
2026-03-09 23:05:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Fibonacci Number/fibonacci-number.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int a = 0, b = 1, c = 0;
+
+        if(n == 0){
+            System.out.println(a);
+        }
+        else if(n == 1){
+            System.out.println(b);
+        }
+        else{
+            for(int i = 2; i <= n; i++){
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            System.out.println(c);
+        }
+    }
+}
