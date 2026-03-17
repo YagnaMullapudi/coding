7b44082b20176e334db3743d1c122ca02ff6841f
YagnaMullapudi <yagnaganeswarimullapudi@gmail.com>
2026-03-17 23:53:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Java loops | Set 1/java-loops-set-1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    static ArrayList<Integer> getSum(int N) {
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for(int i = 1; i <= N; i++) {
+            if(i % 2 == 0) {
+                evenSum += i;
+            } else {
+                oddSum += i;
+            }
+        }
+
+        result.add(evenSum);
+        result.add(oddSum);
+
+        return result;
+    }
+}
